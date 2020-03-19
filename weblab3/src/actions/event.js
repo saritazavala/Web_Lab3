59d5ad2a6f7c806546ec9bf577826a960dbfd999
Sara Zavala <saritazg2010@gmail.com>
2020-03-19 16:26:39 -0600

    Idea is done

---
+++ weblab3/src/actions/event.js
@@ -0,0 +1,1 @@
+//Sara Zavala 18893
--- weblab3/src/components/App/index.js
+++ weblab3/src/components/App/index.js
@@ -1,3 +1,4 @@
+//Sara Zavala 18893
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
